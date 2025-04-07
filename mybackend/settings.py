b0978eb1df2386d07181f03f8a67e1ab3340a4a1
vinc3creed <vince.calo@ustp.edu.ph>
2025-04-07 11:35:51 +0800

    access token backtrack

--- mybackend/settings.py
+++ mybackend/settings.py
@@ -41,19 +41,9 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'myapp',
-    'rest_framework',
-    'rest_framework.authtoken',
 ]
 
-REST_FRAMEWORK = {
-    'DEFAULT_AUTHENTICATION_CLASSES': [
-        'rest_framework.authentication.TokenAuthentication',
-    ],
-    'DEFAULT_PERMISSION_CLASSES': [
-        'rest_framework.permissions.IsAuthenticated',
-    ],
-}
-
+INSTALLED_APPS += ['rest_framework']
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
